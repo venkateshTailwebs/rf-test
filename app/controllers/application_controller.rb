08fbdda6fdfcbcd73a47285168e616a182693d27
giridhar <giridhar@railwebs.com>
2025-03-10 18:17:10 +0530

    removing cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :disable_session, :set_cache_headers_to_public
 
   def disable_session
     # will not set Set-cokkie param on response headers
