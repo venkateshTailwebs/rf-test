99e3ac8fceb02f4d0331ede91e2f3bda0698e5e6
giridhar <giridhar@railwebs.com>
2025-03-10 17:37:45 +0530

    configured to accept all hosts

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module RfTest
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.hosts.all
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
